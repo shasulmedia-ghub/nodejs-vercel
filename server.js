e7236ff64cb704a4609eaf562f72c34f66b6758d
shasulmedia-ghub <shasul.media@gmail.com>
2026-07-18 09:29:45 +0800

    first commit

---
+++ server.js
@@ -0,0 +1,31 @@
+const express = require("express");
+const cors = require("cors");
+
+const app = express();
+
+// Temporary storage of employees
+const employees = [];
+
+//Middleware
+app.use(cors());
+app.use(express.json());
+
+// API end point for saving  employee information
+app.post("/employees", (req, res) => {
+  const newEmployee = req.body;
+  employees.push(newEmployee);
+
+  console.log(newEmployee);
+
+  res
+    .status(201)
+    .json({ success: true, message: "Employee added successfully" });
+});
+
+// adding end point for retrieving all employee information
+app.get("/employees", (req, res) => {
+  res.json(employees);
+});
+
+// export instead of listen
+module.export = app;
